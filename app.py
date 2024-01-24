59b47289b17cc7190387386779a51b80e2e99f85
Finn Mikkola <113478695+ChungoMungus@users.noreply.github.com>
2024-01-24 09:39:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -45,3 +45,26 @@ merged_data_cleaned = merged_data.dropna(subset=['credit_card_number'])
 merged_data_cleaned['credit_card_number'] = merged_data_cleaned['credit_card_number'].astype(str).str.extract('(\d{15,19})')
 
 merged_data_cleaned = merged_data_cleaned.dropna(subset=['credit_card_number'])
+
+
+def phone_cleanup(df):
+    extensions = df['phone_number'].str.extract(r'x(.+)', expand=False)
+    extensions = extensions.fillna('')
+    df['phone_number'] = df['phone_number'].str.replace(r'x(.+)', '')
+    df['phone_number'] = df['phone_number'].str.replace(r'\D', '')
+    for i in range(len(df.phone_number)):
+        if isinstance(df.phone_number[i], str) and df.phone_number[i] is not None:
+            number = ''.join(filter(str.isdigit, df.phone_number[i]))
+            if len(number) == 10:
+                df.phone_number[i] = f'({number[0:3]}) {number[3:6]}-{number[6:10]}'
+            elif len(number) == 11:
+                df.phone_number[i] = f'+{number[0]} ({number[1:4]}) {number[4:7]}-{number[7:11]}'
+            elif len(number) == 12:
+                df.phone_number[i] = f'+{number[0:1]} ({number[2:5]}) {number[5:8]}-{number[8:12]}'
+            elif len(number) == 13:
+                df.phone_number[i] = f'+{number[0:2]} ({number[3:6]}) {number[6:9]}-{number[9:13]}'
+            if str(extensions[i]).isdigit and extensions[i] != '':
+                df.phone_number[i] = df.phone_number[i] + f' x {extensions[i]}'
+            else:
+                pass
+phone_cleanup(df)
